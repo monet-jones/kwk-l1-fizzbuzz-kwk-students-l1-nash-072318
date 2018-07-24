563e7e7f8952cf121c7b8a22dc140f916b9d9367
monet-jones <monet.s.jones@gmail.com>
2018-07-24 18:56:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,12 @@ if number %15 == 0
   puts "FizzBuzz"
 elsif number %5 == 0
   puts "Buzz"
-else number %3 == 0
+elsif number %3 == 0
   puts "Fizz"
+else number 
+  puts 'non'
 end 
 
 end
 
-fizzbuzz(46)
+fizzbuzz(73)
